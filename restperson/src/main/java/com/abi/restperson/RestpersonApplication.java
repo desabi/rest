f21d3d306973ea6a542b046ceb9afa56372d3c06
Abigahil Delgado <abigahil.delgado@ine.mx>
2021-12-07 07:55:49 -0600

    se agrega restperson

---
+++ restperson/src/main/java/com/abi/restperson/RestpersonApplication.java
@@ -0,0 +1,13 @@
+package com.abi.restperson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestpersonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestpersonApplication.class, args);
+	}
+
+}
